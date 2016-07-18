081d0f09808907cbf60f1bf07a50c4e32c155e81
joe <joe>
2016-07-17 20:23:09 -0700

    header template

---
+++ header_template.h
@@ -0,0 +1,17 @@
+/****************************************************
+ * info
+ *****************************************************/
+
+/* Header guard so our header isn't included multiple times at compile. */ 
+#ifndef PASS_GEN_H
+#define PASS_GEN_H
+
+/* Include the standard io library headers we need.*/
+
+/* Global vars
+ */
+
+/* Public functions for this module. */
+
+/* End of header guard. */
+#endif
